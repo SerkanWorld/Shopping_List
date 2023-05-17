e664d367772792fad99424db5a776f6918bbd9f8
Serkan Polat <serkan-polat@gmx.net>
2023-05-17 11:22:50 +0200

    Initial commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { Recipe } from './recipe.model';
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Ravioli',
+      'yummy',
+      'https://images.unsplash.com/photo-1594610352113-ad218529cfb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      [new Ingredient('cheese', 1), new Ingredient('special italian sauce', 2)]
+    ),
+    new Recipe(
+      'spaghetti',
+      'with tomato sauce',
+      'https://images.unsplash.com/photo-1551892374-ecf8754cf8b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      [new Ingredient('spaghetti', 1), new Ingredient('Tomato sauce', 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
